6223abfb604c8e5859ac8abfe581b4e9beff8da6
Salitha Chathuranga <salithachathuranga94@gmail.com>
2018-09-11 17:16:19 +0530

    Add function to change props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,15 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 export class Home extends Component {
+  constructor(props){
+    super();
+    this.name = props.name;
+  }
+
+  changeName(){
+    this.name = "pool";
+    console.log(this.name);
+  }
   render() {
     console.log(this.props);
     return (
@@ -15,6 +24,9 @@ export class Home extends Component {
         <ul className="list-group">
           {this.props.user.hobbies.map((hobby,i) => <li className="list-group-item" key={i}>{hobby}</li>)}
         </ul>
+        <hr/>
+        <p>Change Name : {this.name}</p>
+        <button className="btn btn-primary" onClick={() => this.changeName()}>click me</button>
       </div>
     );
   }
