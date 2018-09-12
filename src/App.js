bef4a442abdfeebbf441e9a986e3f739f0827e43
Salitha Chathuranga <salithachathuranga94@gmail.com>
2018-09-13 01:23:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-// import Home from './components/Home';
 import Form from './components/Form';
 import './App.css';
 
 class App extends Component {
+
   render() {
-    var user = {
-      name: "techpool",
-      job: "Developer",
-      hobbies: ["Reading", "Watching Movies", "Travelling"]
-    }
+
     return (
       <div className="App">
         <Navbar/>
-        {/*<Home name={"salitha"} status={"Undergraduate"} user={user}/>*/}
         <Form/>
       </div>
     );
