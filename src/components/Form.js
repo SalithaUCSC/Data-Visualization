0f9d4b12e68c3ce015e9cb1152ad65af31cb5768
Salitha Chathuranga <salithachathuranga94@gmail.com>
2018-09-12 15:39:20 +0530

    Add modified form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Form extends Component {
   constructor(props){
     super();
     this.state = {
-      products: []
+      products: [],
+      results: [],
+      product_select: "spyglass",
+      test_suite_select: "regress"
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleProdcutChange = this.handleProdcutChange.bind(this);
+    this.handleTestChange = this.handleTestChange.bind(this);
   }
 
   componentDidMount() {
     fetch('http://localhost:4000/api/getProducts')
       .then(res => res.json())
       .then(products => this.setState({ products }));
+  }
+
+  // componentDidMount() {
+  //   axios.get('http://localhost:4000/api/getProducts')
+  //     .then(res => {
+  //       const products = res.data;
+  //       this.setState({products});
+  //     })
+  // }
+
+  // componentWillReceiveProps(props) {
+  //   this.setState(props);
+  // }
+
+  handleSubmit(event) {
+     event.preventDefault();
+     fetch('http://localhost:4000/api/search', {
+       method: "POST",
+       mode: 'cors',
+       headers: {
+         Accept: "Access-Control-Allow-Origin",
+         'Content-Type': 'application/json'
+       },
+       body: {
+         product_select: this.state.product_select,
+         test_suite_select: this.state.test_suite_select
+       }
+
+     }).then((res) => res.json())
+       .then(results => this.setState({ results }))
+ }
+
+
+  handleProdcutChange(event) {
+    this.setState({product_select: event.target.value});
+  }
+
+  handleTestChange(event){
+    this.setState({test_suite_select: event.target.value});
   }
 
   render() {
@@ -22,30 +68,33 @@ class Form extends Component {
     return (
 
       <div className="container" style={style}>
-        <form method="POST" action="">
+        <form onSubmit={this.handleSubmit}>
           <div className="row">
-              <div className="col">
-                  <div className="form-group">
-                      <label>PRODUCT</label>
-                      <select className="form-control" id="product-select" name="product_select">
-                        {this.state.products.map((pro) =>
-                            <option key={pro}>{pro}</option>
-                        )}
-                      </select>
-                  </div>
+          <div className="col">
+              <div className="form-group">
+                  <label>PRODUCT</label>
+                  <select className="form-control" id="product-select" name="product_select" onChange={this.handleProdcutChange}>
+                    {this.state.products.map((pro) =>
+                        <option key={pro} value={pro}>{pro}</option>
+                    )}
+                  </select>
               </div>
-              <div className="col">
-                  <div className="form-group">
-                      <label>TEST SUITE</label>
-                      <select className="form-control" id="test-suite-select" name="test_suite_select">
-                          <option value="regress">regression</option>
-                          <option value="bench">benchmark</option>
-                      </select>
-                  </div>
+          </div>
+          <div className="col">
+              <div className="form-group">
+                  <label>TEST SUITE</label>
+                  <select className="form-control" id="test-suite-select" name="test_suite_select" onChange={this.handleTestChange}>
+                      <option value="regress">regression</option>
+                      <option value="bench">benchmark</option>
+                  </select>
               </div>
+          </div>
           </div>
           <input type="submit" value="submit" className="btn btn-primary" style={{float: 'left'}}/>
         </form>
+        {this.state.results.map((result) =>
+            <option key={result} value={result}>{result}</option>
+        )}
       </div>
 
     );
