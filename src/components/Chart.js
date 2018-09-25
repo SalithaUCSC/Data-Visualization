e1d5bbaae6260057d1e0a8f9657a3f239fd78d75
Salitha Chathuranga <salithachathuranga94@gmail.com>
2018-09-25 16:18:37 +0530

    Modify chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,77 +1,238 @@
 import React, { Component } from 'react';
-import * as d3 from "d3";
-import { ScaleBand, ScaleLinear, schemeCategory20 } from 'd3-scale';
-import { select } from 'd3-selection';
-
-var width = 900;
-var height = 600;
+import axios from 'axios';
+import * as d3 from 'd3';
+import ChildChart from './ChildChart';
+import Test from './Test';
 
 class Chart extends Component {
-
-  componentDidMount(){
-    this.drawChart();
-  }
-
-  drawChart(){
-    const svg = d3.selection("#stacked")
-    const margin = {top: 20, right: 180, bottom: 30, left: 40}
-    const width = +svg.attr("width") - margin.left - margin.right
-    const height = +svg.attr("height") - margin.top - margin.bottom
-    const g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var x = d3.scaleBand().rangeRound([0, width]).padding(0.3).align(0.3);
-
-    var y = d3.scaleLinear().rangeRound([height, 0]);
-
-    var z = d3.scaleOrdinal().range(["red", "green", "blue", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-    var stack = d3.stack();
-
-    d3.json("http://localhost:4000/api/test", function(error, data) {
-      if (error) throw error;
-
-      data.sort(function(a, b) { return b.total - a.total; });
-
-      x.domain(data.map(function(d) { return d.ethnicity; }));
-      y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
-      z.domain(data.columns.slice(1));
-
-      g.selectAll(".serie")
-        .data(stack.keys(data.columns.slice(1))(data))
-        .enter().append("g")
-          .attr("class", "serie")
-          .attr("fill", function(d) { return z(d.key); })
-        .selectAll("rect")
-        .data(function(d) { return d; })
-        .enter().append("rect")
-          .attr("x", function(d) { return x(d.data.ethnicity); })
-          .attr("y", function(d) { return y(d[1]); })
-          .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-          .attr("width", x.bandwidth());
-
-      g.append("g")
-          .attr("class", "axis axis--x")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x));
-    });
-    var t;
-    function type(d, i, columns) {
-      for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-      d.total = t;
-      return d;
+    constructor(props){
+        super(props);
+        this.state = {
+            products: [],
+            results: [],
+            product_select: "spyglass",
+            test_suite_select: "regress"
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleProdcutChange = this.handleProdcutChange.bind(this);
+        this.handleTestChange = this.handleTestChange.bind(this);
     }
-  }
 
-  // var margin = {top: 20, right: 180, bottom: 30, left: 40};
+    componentDidMount() {
+        axios.get('http://localhost:4000/api/products')
+            .then(res => {
+            const products = res.data;
+            this.setState({products});
+        })
+    };
+
+    handleSubmit(event) {
+        event.preventDefault();
+        var config = {
+        headers: {
+            Accept: "Access-Control-Allow-Headers",
+            "Content-Type": "application/json"
+        }
+    };
+    axios.post('http://localhost:4000/api/search',
+    {
+        product_select: this.state.product_select,
+        test_suite_select: this.state.test_suite_select
+    }, config)
+        .then(res => {
+            console.log(res);
+            this.setState({results: res.data.objArr});
+            console.log(this.state.results);
+            this.drawChart(this.state.results);          
+            
+        })
+    }
 
 
-  render() {
-    return (
-      <svg id="stacked" height={height} width={width}>
-        <g transform={`translate(40,20)`}></g>
-      </svg>
+    handleProdcutChange(event) {
+        this.setState({product_select: event.target.value});
+    }
 
-  );
+    handleTestChange(event){
+        this.setState({test_suite_select: event.target.value});
+    };
+
+    updateChart(){
+        console.log('update')
+        // this.drawChart(this.state.results);
+    }
+
+    drawChart(data){
+
+        var margin = {top: 20, right: 160, bottom: 35, left: 30};
+        var width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var svg = d3.select("#chart")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // Transpose the data into layers
+        var dataset = d3.layout.stack()(["nodata", "nolevel"].map(function(y) {
+            return data.map(function(d) {
+                return {x: d.version, y: +d[y]};
+            });
+
+        }));
+        console.log(dataset)
+
+        // Set x, y and colors
+        var x = d3.scale.ordinal()
+            .domain(dataset[0].map(function(d) { console.log(d.x); return d.x; }))
+            .rangeRoundBands([0, width], 0.05);
+
+        var y = d3.scale.linear()
+            .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
+            .range([height, 0]);
+
+        var colors = ["#63C09F", "#55A8BC"];
+
+        // Define and draw axes
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .ticks(10)
+            .tickSize(0.5, 0.5)
+            .tickFormat( function(d) { return d } );
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .tickSize(0.5, 0.5)
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Create groups for each series, rects for each segment
+        var groups = svg.selectAll("g.product-group")
+            .data(dataset)
+            .enter().append("g")
+            .attr("class", "product-group")
+            .style("fill", function(d, i) { return colors[i]; });
+
+        var rect = groups.selectAll("rect")
+            .data(function(d) { return d; })
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return x(d.x); })
+            .attr("y", function(d) { return y(d.y0 + d.y); })
+            .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
+            .attr("width", x.rangeBand)
+
+        var legend = svg.selectAll(".legend")
+            .data(colors)
+            .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(30," + i * 19 + ")"; });
+
+        legend.append("rect")
+            .attr("x", width - 18)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", function(d, i) {return colors.slice().reverse()[i];});
+
+        legend.append("text")
+            .attr("x", width + 5)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "start")
+            .text(function(d, i) {
+            switch (i) {
+                case 0: return "nolevel";
+                case 1: return "nodata";
+            }
+        });
+
+    };
+
+    render() {
+        
+        var style = {
+            marginTop: '80px'
+    };
+
+    
+    return (    
+
+        <div className="container" style={style}>
+        <form onSubmit={this.handleSubmit}>
+                <div className="row">
+                    <div className="col">
+                        <div className="form-group">
+                            <label>PRODUCT</label>
+                            <select className="form-control" id="product-select" name="product_select" onChange={this.handleProdcutChange}>
+                            {this.state.products.map((pro) =>
+                                <option key={pro} value={pro}>{pro}</option>
+                            )}
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="col">
+                        <div className="form-group">
+                            <label>TEST SUITE</label>
+                            <select className="form-control" id="test-suite-select" name="test_suite_select" onChange={this.handleTestChange}>
+                                <option value="regress">regression</option>
+                                <option value="bench">benchmark</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>    
+                <input type="submit" value="submit" className="btn btn-primary" style={{float: 'left'}}/>
+            </form>
+            <br/>
+            <br />
+            <br />
+            <hr />
+            <div className="row">
+                <table className="table table-striped table-bordered">
+                    <thead>
+                    <tr>
+                      <th>version</th>
+                      <th>nodata</th>
+                      <th>nolevel</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.results.map((pro, i) => {
+                          return (
+                            <tr key={i.toString()}>
+                              <td>{pro.version}</td>
+                              <td>{pro.nodata}</td>
+                              <td>{pro.nolevel}</td>
+                            </tr>
+                          )
+                        }
+                      )}
+                    </tbody>
+                </table>
+                
+            </div>
+            <div className="row">
+              <div id="chart">
+                    {/* <ChildChart data={this.state.results} drawChart={this.drawChart}/> */}
+              </div>
+            </div>  
+            <div>
+                {/* <Test data={this.state.results}/> */}
+            </div>      
+        </div>
+        );
+    }
 }
-}
-export default Chart;
+
+export default Chart;   
