5bc3009c59f10d8c77861c5bee7050edbdea0428
Salitha Chathuranga <salithachathuranga94@gmail.com>
2018-10-08 12:20:30 +0530

    Refactor the code and add comments

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,96 +1,108 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import * as d3 from 'd3';
-import * as tip from 'd3-tip/dist/index'
 import './Chart.css'
 
 class Chart extends Component {
     constructor(props){
         super(props);
+        // Define the state for the component
         this.state = {
             products: [],
             results: [],
             product_select: "spyglass",
-            test_suite_select: "regress",
-            chart: ''
+            test_suite_select: "regress"
         }
+        // Bind the functions 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleProdcutChange = this.handleProdcutChange.bind(this);
         this.handleTestChange = this.handleTestChange.bind(this);
+        this.drawChart = this.drawChart.bind(this);
     }
 
+    // Life cycle hook to set the product names once the component is loaded
     componentDidMount() {
+        // Get product names dynamically from database
         axios.get('http://localhost:4000/api/products')
             .then(res => {
-            const products = res.data;
-            this.setState({products});
+            // Set the product names to the state array called products
+            this.setState({products: res.data});
         })
     };
-    
+
+    // Function to send the input data to the backend
     handleSubmit(event) {
         event.preventDefault();
         var config = {
         headers: {
+            // Set headers to avoid errors in fetching data
             Accept: "Access-Control-Allow-Headers",
             "Content-Type": "application/json"
         }
     };
+    // Send a POST request
     axios.post('http://localhost:4000/api/search',
     {
+        // Send the data to the serevr for a POST request
         product_select: this.state.product_select,
         test_suite_select: this.state.test_suite_select
     }, config)
         .then(res => {
-            console.log(res);
+            // Set the data to the state array called results
             this.setState({results: res.data.objArr});
-            console.log(this.state.results);
-            // this.setState({chart: this.drawChart(this.state.results)});
+            // Call drawChart function to draw the chart using incoming data
             this.drawChart(this.state.results);
-
         })
     }
 
-
+    // Function to handle input changes in product selection
     handleProdcutChange(event) {
         this.setState({product_select: event.target.value});
     }
 
+    // Function to handle input changes in suite selection
     handleTestChange(event){
         this.setState({test_suite_select: event.target.value});
     };
 
+    // Function to draw the chart using fetched data from MongoDB
     drawChart(data){
-
+        var chart = document.getElementById("chart");
+        
+        // check the HTML code stored in chart
+        if(chart.innerHTML !== ""){
+            // if it's not empty, make it empty - replace the chart when state is changed
+            chart.innerHTML = "";
+        }
+        // set the margins for the svg and decide its dimensions
         var margin = {top: 100, right: 160, bottom: 100, left: 70};
         var width = 1200 - margin.left - margin.right,
             height = 1000 - margin.top - margin.bottom;
 
+        // select the section with id chart from DOM and add the svg with dimensions
         var svg = d3.select("#chart")
             .append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + (margin.left+30) + "," + margin.top + ")");
-
-        // Transpose the data into layers
-        var dataset = d3.layout.stack()(["nolevel","nodata"].map(function(y) {
-            return data.map(function(d) {
-                
-                return {x: d.version, y: +d[y]};
+        
+        // Transpose the data into stacks
+        var dataset = d3.layout.stack()(["dataType","nodataType"].map(function(y) {
+            return data.map(function(d) {              
+                return {x: d.category, y: +d[y]};
             });
-
         }));
-        console.log("dataset : ", dataset)
 
         // Set x, y and colors
         var x = d3.scale.ordinal()
             .domain(dataset[0].map(function(d) { return d.x; }))
             .rangeRoundBands([0, width], 0.05);
-
+        // Define data for y axis
         var y = d3.scale.linear()
             .domain([0, d3.max(dataset, function(d) {
                 return d3.max(d, function(d) {
-                    // enhance dataset output
+                    // enhance dataset output, if a collection count is not found
                     for (var i = 0; i < dataset.length; i++) {
                         for (var j = 0; j < dataset[i].length; j++) {
                             if (!(dataset[i][j].y > 0)) {
@@ -106,24 +118,26 @@ class Chart extends Component {
             })])
             .range([height, 0]);
 
+        // Define the colors for stacked bar chart
         var colors = ["#3498DB", "#1ABC9C"];
 
-        // Define and draw axes
+        // Define and draw y axis
         var yAxis = d3.svg.axis()
             .scale(y)
             .orient("left")
             .ticks(10)
             .tickSize(-width, 0.5, 0.5)
             .tickPadding(10)
-            .tickFormat( function(d) { return d } );
+            .tickFormat( function(d) { return d } );    
 
+        // Define and draw x axis
         var xAxis = d3.svg.axis()
             .scale(x)
             .orient("bottom")
             .tickSize(-width, 0.5, 0.5)
-            .tickPadding(10)
+            .tickPadding(10)          
 
-
+        // Append the y axis to the svg
         svg.append("g")
             .attr("class", "y axis")
             .call(yAxis)
@@ -134,6 +148,7 @@ class Chart extends Component {
             .style("text-anchor", "end")
             .text("Total Count");
 
+        // Append the x axis to the svg
         svg.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
@@ -150,6 +165,7 @@ class Chart extends Component {
             .attr("class", "product-group")
             .attr("fill", function(d, i) { return colors[i]; })
 
+        // Create stacked group columns
         groups.selectAll("rect")
             .data(function(d) { return d; })
             .enter()
@@ -159,28 +175,22 @@ class Chart extends Component {
             .attr("y", function(d) { return y(d.y0 + d.y); })
             .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
             .attr("width", x.rangeBand)
-            .on("mouseover", function() { tooltip.style("display", null); })
-            .on("mouseout", function() { tooltip.style("display", "none"); })
-            .on("mousemove", function(d) {
-              var xPosition = d3.mouse(this)[0] - 15;
-              var yPosition = d3.mouse(this)[1] - 25;
-              tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-              tooltip.select("text").text(d.y);
-            });
 
+        // Define the legend and its placement
         var legend = svg.selectAll(".legend")
             .data(colors)
             .enter().append("g")
             .attr("class", "legend")
             .attr("transform", function(d, i) { return "translate(30," + i * 19 + ")"; });
 
+        // Draw legend key boxes
         legend.append("rect")
             .attr("x", width - 18)
             .attr("width", 18)
             .attr("height", 18)
             .attr("fill", function(d, i) {return colors.slice().reverse()[i];})
 
-
+        // Add legend text
         legend.append("text")
             .attr("x", width + 5)
             .attr("y", 9)
@@ -190,43 +200,31 @@ class Chart extends Component {
             switch (i) {
                 case 0: return "nodata";
                 case 1: return "data";
+                default: return "not match"
             }
         });
 
+        // Add the title to the chart
         svg.append("text")
             .attr("x", (width / 2))
             .attr("y", 0 - (margin.top / 3))
             .attr("text-anchor", "middle")
             .style("font-size", "20px")
             .text("Test Results");
-
-        var tooltip = svg.append("g")
-            .attr("class", "tooltip")
-            .style("display", "none");
-              
-        var toolRect =   tooltip.append("rect")
-            .attr("width", 30)
-            .attr("height", 20)
-            .attr("fill", "white")
-            .style("opacity", 0.5);
-          
-            toolRect.append("text")
-            .attr("x", -15)
-            .attr("y", height)
-            .attr("dy", "1.2em")
-            .style("text-anchor", "middle")
-            .attr("font-size", "12px")
-            .attr("font-weight", "bold");
+           
+        // return the final svg to the DOM
+        return svg;
 
     };
 
     render() {
-
+        // Define some styling
         var style = {
             marginTop: '80px',
             minHeight: '3000px'
     };
-    
+
+    // Define data array
     var data = this.state.results;
 
     return (
@@ -256,7 +254,7 @@ class Chart extends Component {
                         </div>
                     </div>
                 </div>
-                <input type="submit" value="Find Results" className="btn btn-primary" style={{float: 'left'}}/>
+                <input type="submit" value="Find Results" className="btn btn-primary btn-sm" style={{float: 'left', marginLeft: '0px'}}/>
             </form>
             <br/>
             <br />
@@ -279,16 +277,16 @@ class Chart extends Component {
                         {data.length > 0 ? 
 
                         data.map((pro, i) => { 
-                            pro.nodata = pro.nodata || 0;
-                            pro.nolevel = pro.nolevel || 0;
+                            pro.nodataType = pro.nodataType || 0;
+                            pro.dataType = pro.dataType || 0;
                            
                             return (
 
                                 <tr key={i.toString()} className="animated bounceIn">
-                                    <td>{pro.version}</td>
-                                    <td>{!(pro.nolevel>0) ? 0 : pro.nolevel}</td>
-                                    <td>{!(pro.nodata>0) ? 0 : pro.nodata}</td>
-                                    <td>{pro.nolevel + pro.nodata}</td>
+                                    <td>{pro.category}</td>
+                                    <td>{!(pro.dataType>0) ? 0 : pro.dataType}</td>
+                                    <td>{!(pro.nodataType>0) ? 0 : pro.nodataType}</td>
+                                    <td>{pro.dataType + pro.nodata}</td>
                                 </tr>
 
                             )}
@@ -298,8 +296,7 @@ class Chart extends Component {
                 </table>
             </div>
             <div className="row">
-                <div id="chart" className="chart animated bounceIn">
-            {/* {data.length > 0 ? (<ChildChart data={this.state.results}/>) : null} */}
+                <div id="chart" className="chart animated bounceIn"> 
                 
                 </div> 
             </div>
